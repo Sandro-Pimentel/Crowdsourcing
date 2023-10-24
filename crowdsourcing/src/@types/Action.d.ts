a775eefd2c1831e1caeee99fe8f8d60e13c483b1
Sandro-Pimentel <125677890+Sandro-Pimentel@users.noreply.github.com>
2023-10-23 21:44:24 -0300

    Adicionando Action.d.ts(interfaces/types)

---
+++ crowdsourcing/src/@types/Action.d.ts
@@ -0,0 +1,16 @@
+declare interface IAction {
+    img?: string,
+    title?: string,
+    author?: string, 
+    description?: string,
+    address?: AddressType,
+    amountCollected?: number,
+    totalGoal?: number,
+    finalDate?: string
+}
+
+declare type AddressType = {
+    uf?: string,
+    city?: string, 
+    neighborhood?: string
+}
